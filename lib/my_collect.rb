8221076742e8f639abc27a4ce27948cb8bb12e2c
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-11 23:55:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,14 @@
-def my_collect (collection)
-  
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection << yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
 end
+
+my_collect(list) {|i| i.split(" ").first}
 
